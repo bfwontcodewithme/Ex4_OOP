dcd16ef1a94bee9455a956efd4f41ceedeb630db
tomas <tomtom.inbar@gmail.com>
2022-01-05 13:23:15 +0200

    Added information to the gui: button to quit 'gracefully' w/e that means points gained moves made time left in seconds

--- client_python/student_code.py
+++ client_python/student_code.py
@@ -3,6 +3,7 @@
 OOP - Ex4
 Very simple GUI example for python client to communicates with the server and "play the game!"
 """
+import sys
 from types import SimpleNamespace
 from client import Client
 import json
@@ -43,7 +44,7 @@ for n in graph.Nodes:
     x, y, _ = n.pos.split(',')
     n.pos = SimpleNamespace(x=float(x), y=float(y))
 
- # get data proportions
+# get data proportions
 min_x = min(list(graph.Nodes), key=lambda n: n.pos.x).pos.x
 min_y = min(list(graph.Nodes), key=lambda n: n.pos.y).pos.y
 max_x = max(list(graph.Nodes), key=lambda n: n.pos.x).pos.x
@@ -55,7 +56,7 @@ def scale(data, min_screen, max_screen, min_data, max_data):
     get the scaled data with proportions min_data, max_data
     relative to min and max screen dimentions
     """
-    return ((data - min_data) / (max_data-min_data)) * (max_screen - min_screen) + min_screen
+    return ((data - min_data) / (max_data - min_data)) * (max_screen - min_screen) + min_screen
 
 
 # decorate scale with the correct values
@@ -64,7 +65,7 @@ def my_scale(data, x=False, y=False):
     if x:
         return scale(data, 50, screen.get_width() - 50, min_x, max_x)
     if y:
-        return scale(data, 50, screen.get_height()-50, min_y, max_y)
+        return scale(data, 50, screen.get_height() - 50, min_y, max_y)
 
 
 radius = 15
@@ -83,6 +84,7 @@ The GUI and the "algo" are mixed - refactoring using MVC design pattern is requi
 """
 
 while client.is_running() == 'true':
+
     pokemons = json.loads(client.get_pokemons(),
                           object_hook=lambda d: SimpleNamespace(**d)).Pokemons
     pokemons = [p.Pokemon for p in pokemons]
@@ -93,18 +95,49 @@ while client.is_running() == 'true':
     agents = json.loads(client.get_agents(),
                         object_hook=lambda d: SimpleNamespace(**d)).Agents
     agents = [agent.Agent for agent in agents]
+
     for a in agents:
         x, y, _ = a.pos.split(',')
         a.pos = SimpleNamespace(x=my_scale(
             float(x), x=True), y=my_scale(float(y), y=True))
+
     # check events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+            pygame.quit()
+            exit(0)
+            client.stop_connection()
 
     # refresh surface
     screen.fill(Color(0, 0, 0))
+
+    info = client.get_info()
+    info_json = json.loads(info)
+
+    # draw information
+    x = screen.get_width() * 0.08
+    y = screen.get_height() * 0.025
+    text_info = FONT.render('press q to quit', True, Color(255, 255, 255))
+    text_rect = text_info.get_rect(center=(x, y))
+    screen.blit(text_info, text_rect)
+
+    y = screen.get_height() * 0.055
+    text_info = FONT.render('Points:' + str(info_json['GameServer']['grade']), True, Color(255, 255, 255))
+    text_rect = text_info.get_rect(center=(x, y))
+    screen.blit(text_info, text_rect)
+
+    y = screen.get_height() * 0.085
+    text_info = FONT.render('Moves made:' + str(info_json['GameServer']['moves']), True, Color(255, 255, 255))
+    text_rect = text_info.get_rect(center=(x, y))
+    screen.blit(text_info, text_rect)
+
+    y = screen.get_height() * 0.115
+    text_info = FONT.render('Time left(s):' + str(int(client.time_to_end()) / 1000), True, Color(255, 255, 255))
+    text_rect = text_info.get_rect(center=(x, y))
+    screen.blit(text_info, text_rect)
 
     # draw nodes
     for n in graph.Nodes:
@@ -157,7 +190,7 @@ while client.is_running() == 'true':
         if agent.dest == -1:
             next_node = (agent.src - 1) % len(graph.Nodes)
             client.choose_next_edge(
-                '{"agent_id":'+str(agent.id)+', "next_node_id":'+str(next_node)+'}')
+                '{"agent_id":' + str(agent.id) + ', "next_node_id":' + str(next_node) + '}')
             ttl = client.time_to_end()
             print(ttl, client.get_info())
 
